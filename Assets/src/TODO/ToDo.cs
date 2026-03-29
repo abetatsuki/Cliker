dbdbeffeffa2ae9c4edeb0b2a9e9992dc9544a25
kenkatou354 <kenkatou354@gmail.com>
2026-03-29 19:59:38 +0900

    [add] Todoフォルダを追加。

---
+++ Assets/src/TODO/ToDo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ToDo : MonoBehaviour
+{
+    //TODO : PlayerSaveData を作る
+    //TODO : PlayerData を集約ルートとして作る
+    //TODO : IPlayerRepository を作る
+    //TODO : PlayerRepository を作る
+    //TODO : DataManager を PlayerRepository に置き換える
+}
