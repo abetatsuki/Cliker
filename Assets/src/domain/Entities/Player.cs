3f59dca2dec5959e843813e6a2cec6dd6a8d3eea
kenkatou354 <kenkatou354@gmail.com>
2026-04-01 22:27:53 +0900

    [add] プレイヤーの操作の整合性を保つために集合ルートを追加

---
+++ Assets/src/domain/Entities/Player.cs
@@ -0,0 +1,48 @@
+﻿
+using Src.Domain.ValueObjects;
+namespace Src.Domain.Entities
+{
+    /// <summary>
+    /// プレイヤールート
+    /// </summary>
+    public class Player : IEntity
+    {
+        /// <summary>
+        /// 所持金を増加させる。
+        /// </summary>
+        public void AddMoney(Money amount)
+        {
+            _money.Add(amount);
+        }
+        public bool TrySubtract(Money cost)
+        {
+            if (!_money.CanSubtract(cost))return false;
+            _money.Subtract(cost);
+            return true;
+        }
+        public void Click()
+        {
+            _money.Add(new Money(1));
+        }
+        public bool TryBuy(Money cost)
+        {
+            if (!_money.CanSubtract(cost)) return false;
+            _money.Subtract(cost);
+            return true;
+        }
+        public void ChangeName(Name name)
+        {
+            if (name == _name) return;
+            _name = name;
+        }
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+        public override int Id => _id;
+
+        private  int _id;
+        private Money _money;
+        private Name _name;
+    }
+}
