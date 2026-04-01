f7a97beb488edc006dbddea3d4ae7a80a2945184
kenkatou354 <kenkatou354@gmail.com>
2026-04-01 23:17:40 +0900

    [add] Jsonに保存するためにリポジトリクラスを追加

---
+++ Assets/src/Infra/Json/JsonPlayerRepository.cs
@@ -0,0 +1,30 @@
+﻿
+using Src.Domain.Entities;
+using System.IO;
+using UnityEditor.Playables;
+using UnityEngine;
+
+namespace Src.Domain.Repositories
+{ 
+    public class JsonPlayerRepository : IPlayerRepository
+    {
+        public JsonPlayerRepository(string path)
+        {
+            _path = path;
+        }
+
+        public Player Find(int playerId)
+        {
+            var json = File.ReadAllText(_path);
+            var player = JsonUtility.FromJson<Player>(json);
+
+            return player;
+        }
+        public void Save(Player player)
+        {
+
+        }
+
+        private readonly string _path;
+    }
+}
