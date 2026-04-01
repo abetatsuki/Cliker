d4fc35e539b84404073b41650631110593ade7ad
kenkatou354 <kenkatou354@gmail.com>
2026-04-01 22:27:03 +0900

    [add] プレイヤーデータの永続化のためリポジトリクラスを追加

---
+++ Assets/src/domain/Repositories/Interface/IPlayerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Src.Domain.Entities;
+
+namespace Src.Domain.Repositories
+{
+    public interface IPlayerRepository
+    {
+        Player Find(int playerId);
+        void Save(Player player);
+    }
+}
---
+++ Assets/src/domain/Repositories/PlayerRepository.cs
@@ -0,0 +1,22 @@
+﻿
+using Src.Domain.Entities;
+
+namespace Src.Domain.Repositories
+{ 
+    public class PlayerRepository : IPlayerRepository
+    {
+        public PlayerRepository()
+        {
+
+        }
+
+        public Player Find(int playerId)
+        {
+            return new Player();
+        }
+        public void Save(Player player)
+        {
+
+        }
+    }
+}
