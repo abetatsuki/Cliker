5b5ae3150433a63119b84d7d379037bbd97a41dd
kenkatou354 <kenkatou354@gmail.com>
2026-04-04 14:23:20 +0900

    [add] Jsonで保存するためのデータを追加

---
+++ Assets/src/Infra/Json/PlayerDto.cs
@@ -0,0 +1,18 @@
+﻿using Src.Domain.Entities;
+
+namespace Src.Infra.Json
+{
+    public class PlayerDto
+    {
+        public int Id;
+        public string Name;
+        public int Money;
+
+        public static PlayerDto ToDto(Player player) => new PlayerDto
+        {
+            Id = player.Id.Value,
+            Name = player.Name.Value,
+            Money = player.Money.Amonut
+        };
+    }
+}
