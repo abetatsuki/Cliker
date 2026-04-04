6c9c80f33502718b1945d58958bc1b1db6f4fece
kenkatou354 <kenkatou354@gmail.com>
2026-04-04 14:22:55 +0900

    [add] プレイヤーの生成パターンが複数あるためファクトリーでカプセル化

---
+++ Assets/src/Infra/Json/PlayerFactory.cs
@@ -0,0 +1,23 @@
+﻿using Src.Domain.Entities;
+using Src.Domain.Factories;
+using Src.Domain.ValueObjects;
+public class PlayerFactory : IPlayerFactroy
+{
+    public Player CreateNormal(PlayerId id,Name name,Money money)
+    {
+        return new Player(
+            id,
+            name,
+            money = default
+            );
+    }
+
+    public Player CreateVip(PlayerId id , Name name, Money money)
+    {
+        return new Player(
+            id,
+            name,
+            money.Add(new Money(1000))
+            );
+    }
+}
