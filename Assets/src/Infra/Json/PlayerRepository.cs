5d788e08180d8a0975ae2a11e7d08535620d12fd
kenkatou354 <kenkatou354@gmail.com>
2026-04-04 14:22:03 +0900

    [add] プレイヤーのデータ保存、ロード機能を追加

---
+++ Assets/src/Infra/Json/PlayerRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using Src.Domain.Entities;
+using System.IO; 
+using UnityEngine;
+using Src.Domain.Repositories;
+using Src.Domain.Factories;
+
+namespace Src.Infra.Json
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        public PlayerRepository(IPlayerFactroy factory)
+        {
+            _saveDir = DataPathUtility.PlayersPath;
+            Directory.CreateDirectory(_saveDir);
+        }
+
+        public Player Find(PlayerId id)
+        {
+            string path = DataPathUtility.GetPath(id);
+            if(!File.Exists(path)) return null;
+
+            var dto = JsonUtility.FromJson<PlayerDto>(File.ReadAllText(path));
+
+            return JsonUtility.FromJson<Player>(json);
+        }
+        public void Save(Player player)
+        {
+            var dto = PlayerDto.ToDto(player);
+            string json = JsonUtility.ToJson(dto, prettyPrint: true);
+            string path = DataPathUtility.GetPath(player.Id);
+            File.WriteAllText(path, json);
+        }
+
+        private readonly string _saveDir;
+    }
+}
